66a0ed90ee63d472b212f8a9987ced4264b3ede2
DESKTOP-FP75N25\ndanl <ndanl4647@gmail.com>
2021-03-11 22:39:54 +0900

    필요 없는 import 제거

--- src/com/som/problem/programmers/AthleteCantFinish.java
+++ src/com/som/problem/programmers/AthleteCantFinish.java
@@ -1,14 +1,11 @@
 package com.som.problem.programmers;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class AthleteCantFinish {
 	// https://programmers.co.kr/learn/courses/30/lessons/42576?language=java
 	
-	// 비완주자가 여러명이 될 수도 있는 경우
 	public String solution(String[] participant, String[] completion) {
 		String answer = "";
 		Map<String, Integer> participantMap = new HashMap<>();
