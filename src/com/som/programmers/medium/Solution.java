f6313283b908a0fa6ae3d3f2464e3597925d9836
Sumsan38 <ndanl4647@gmail.com>
2021-03-16 23:54:32 +0900

    3st Commit - Problem being solved https://programmers.co.kr/learn/courses/30/lessons/42577

---
+++ src/com/som/programmers/medium/Solution.java
@@ -0,0 +1,21 @@
+package com.som.programmers.medium;
+
+import java.util.Vector;
+/**
+ * @author ndanl
+ * https://programmers.co.kr/learn/courses/30/lessons/42577
+ */
+public class Solution {
+	public boolean solution(String[] phone_book) {
+		boolean answer = true;
+		Vector<String> phoneVector = new Vector<>();
+		for (String phone : phone_book) {
+			
+		}
+		
+		
+		
+		return answer;
+	}
+
+}
