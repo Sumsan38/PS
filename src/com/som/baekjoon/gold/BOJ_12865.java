5d9f01ae4cc7ac241c757b71bb31e2fa98000aac
Sumsan38 <ndanl4647@gmail.com>
2022-04-20 17:06:23 +0900

    [백준, 평범한 배낭] 풀이중

---
+++ src/com/som/baekjoon/gold/BOJ_12865.java
@@ -0,0 +1,70 @@
+package com.somi.boj;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+/*
+ * https://www.acmicpc.net/problem/12865
+ * 평범한 배낭
+ * 2022.04.20 16:30 ~ 
+ * */
+public class BOJ_12865 {
+	
+	static int bestV = Integer.MIN_VALUE;
+	static int n;
+	static int k;
+	
+	static boolean choose[];
+	static int[] itemW;
+	static int[] values;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer token;
+		token = new StringTokenizer(br.readLine(), " ");
+		n = Integer.parseInt(token.nextToken()); // 물건의 수 N개
+		k = Integer.parseInt(token.nextToken()); // 준서가 버틸 수 있는 무게 K
+		
+		itemW = new int[n];
+		values = new int[n];
+		choose = new boolean[n];
+		
+		for (int i = 0; i < itemW.length; i++) {
+			token = new StringTokenizer(br.readLine(), " ");
+			itemW[i] = Integer.parseInt(token.nextToken());
+			values[i] = Integer.parseInt(token.nextToken());
+		}
+		
+		chooseItem(0, 0, 0);
+		System.out.println(bestV);
+		
+	}
+	
+	// 브루트포스로 푸는 방법은 시간초과
+	private static void chooseItem(int totalweight, int totalvalue, int beforeChoose){
+		for (int i = beforeChoose; i < choose.length; i++) {
+			int value = totalvalue + values[i];
+			int weight = totalweight + itemW[i];
+			choose[i] = true;
+			
+			if(weight <= k) {
+				bestV = Math.max(bestV, value);
+				chooseItem(weight, value, i + 1);
+			} else {
+				continue;
+			}
+			
+			// 백트레킹으로 고르지 않은 것으로 되돌린다
+			choose[i] = false;
+		}
+	}
+	
+	
+	
+}
